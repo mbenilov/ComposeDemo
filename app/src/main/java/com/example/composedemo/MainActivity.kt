a1e339a26f96ab0c5adc79f5ef400e9118f236b6
Mark Benilov <mark.benilov@gmail.com>
2024-02-05 13:20:20 -0500

    enable edge to edge display

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.composedemo
 
+
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,12 +41,15 @@ import com.example.composedemo.ui.theme.GreenSpotify
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             ComposeDemoTheme {
                 Surface(color = GreenSpotify) {
                     Column(
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .statusBarsPadding()
+                            .navigationBarsPadding(),
                         verticalArrangement = Arrangement.Top,
                     ) {
 //                        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
@@ -49,18 +72,24 @@ fun SearchBar() {
             .fillMaxWidth()
             .padding(12.dp),
         colors = TextFieldDefaults.textFieldColors(
-            textColor = Black,
-            cursorColor = Black,
+            textColor = White,
+            cursorColor = White,
             backgroundColor = Transparent,
-            focusedIndicatorColor = Black,
-            unfocusedIndicatorColor = Black,
+            focusedIndicatorColor = White,
+            unfocusedIndicatorColor = White,
         ),
         textStyle = TextStyle(fontSize = 20.sp),
         trailingIcon = {
-            Icon(
-                Icons.Default.Search,
-                contentDescription = "Search",
-                tint = Black
+            val context = LocalContext.current
+            IconButton(
+                onClick = { Toast.makeText(context, text, Toast.LENGTH_SHORT).show() },
+                content = {
+                    Icon(
+                        Icons.Default.Search,
+                        contentDescription = "Search",
+                        tint = White
+                    )
+                }
             )
         }
     )
